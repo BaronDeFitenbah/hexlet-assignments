3321dcacaf6b810d04eb9d402bbcca8183d9da7f
@hexlet/cli <support@hexlet.io>
2024-05-01 21:43:32 +0500

    submit java-spring-ru/jpa-comparing-entities

--- java-spring-ru/jpa-comparing-entities/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/jpa-comparing-entities/src/main/java/exercise/controller/ProductsController.java
@@ -1,6 +1,7 @@
 package exercise.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,14 +32,13 @@ public class ProductsController {
         return productRepository.findAll();
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(path = "")
     public Product create(@RequestBody Product product) {
         var dopplegunger = productRepository.findAll().stream()
                 .filter(p -> p.equals(product))
                 .toList();
         if (dopplegunger.isEmpty()) {
-            return product;
+            return productRepository.save(product);
         } else {
             throw new ResourceAlreadyExistsException("");
         }
