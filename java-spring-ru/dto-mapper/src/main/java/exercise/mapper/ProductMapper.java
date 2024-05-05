ecd76dbcc0719663ca722af535423c7307abe5de
@hexlet/cli <support@hexlet.io>
2024-05-05 22:14:47 +0500

    submit java-spring-ru/dto-mapper

--- java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
+++ java-spring-ru/dto-mapper/src/main/java/exercise/mapper/ProductMapper.java
@@ -19,11 +19,11 @@ import exercise.model.Product;
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public abstract class ProductMapper {
-    @Mapping(target = "price", source = "cost")
-    @Mapping(target = "title", source = "name")
+    @Mapping(target = "cost", source = "price")
+    @Mapping(target = "name", source = "title")
     public abstract Product map(ProductCreateDTO dto);
-    @Mapping(target = "price", source = "cost")
-    @Mapping(target = "title", source = "name")
+    @Mapping(target = "cost", source = "price")
+    @Mapping(target = "name", source = "title")
     public abstract ProductDTO map(Product model);
     @Mapping(target = "cost", source = "price")
     public abstract void update(ProductUpdateDTO dto, @MappingTarget Product model);
