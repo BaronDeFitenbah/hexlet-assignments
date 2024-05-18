b9fbe33bcfa36e30bc314b066eec2ba9e1fd7db7
@hexlet/cli <support@hexlet.io>
2024-05-18 20:52:02 +0500

    submit java-spring-ru/authentication

--- java-spring-ru/authentication/src/main/java/exercise/config/SecurityConfig.java
+++ java-spring-ru/authentication/src/main/java/exercise/config/SecurityConfig.java
@@ -41,9 +41,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         // Разрешаем доступ только к /api/login, чтобы аутентифицироваться и получить токен
-                        .requestMatchers("/api/login", "users").permitAll()
-                        .anyRequest().authenticated()
-                        .anyRequest().hasAuthority("ROLE_ADMIN"))
+                        .requestMatchers("/api/login", "/users").permitAll()
+                        .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer((rs) -> rs.jwt((jwt) -> jwt.decoder(jwtDecoder)))
                 .httpBasic(Customizer.withDefaults())
